975b00277b8da399acd8e5db18b7b41c0d02b607
fyxw <37008822+fyxw@users.noreply.github.com>
2022-09-17 23:57:04 +0800

    修改dashboard; 增加which-key提示

--- lua/conf/alpha.lua
+++ lua/conf/alpha.lua
@@ -1,1 +1,19 @@
-require'alpha'.setup(require'alpha.themes.dashboard'.config)
+-- require'alpha'.setup(require'alpha.themes.dashboard'.config)
+local dashboard = require('alpha.themes.dashboard')
+
+local plugins = '~/.config/nvim/lua/plugins.lua'
+if vim.fn.has('win64') == 1 then
+    plugins = '~/AppData/Local/nvim/lua/plugins.lua<cr>'
+end
+
+dashboard.section.buttons.val = {
+  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+  -- dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", " " .. " Plugins", ":e " .. plugins .. " <CR>"),
+  dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+}
+
+require('alpha').setup(dashboard.config)
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,25 +15,27 @@ map('', 'Q', ':q<cr>', {})
 map('', 'S', ':w<cr>', {})
 
 -- About buffers
-map('n', ',,', '<cmd>vsp<cr>', {})
-map('n', '<leader>[]', '<cmd>tabedit<cr>', {})
+map('n', '<leader>|', '<cmd>vsp<cr>', {})
+map('n', '<leader>[]', '<cmd>tabedit<cr>', { desc = 'edit' })
 map('n', '{', '<cmd>bp<cr>', {})
 map('n', '}', '<cmd>bn<cr>', {})
 map('n', 'tx', '<cmd>bd<cr>', {})
 map('n', 'H', '<c-w>h', {})
-map('n', 'I', '<c-w>l', {})
+map('n', 'L', '<c-w>l', {})
 
 -- Quickly edit the configuration
+local opts = { desc = 'edit plugins.lua' }
 if vim.fn.has('win64') == 1 then
-    map('n', '<leader>fs', '<cmd>tabedit ~/AppData/Local/nvim/lua/plugins.lua<cr>', {}) else
-    map('n', '<leader>fs', '<cmd>tabedit ~/.config/nvim/lua/plugins.lua<cr>', {})
+    map('n', '<leader>fs', '<cmd>tabedit ~/AppData/Local/nvim/lua/plugins.lua<cr>', opts)
+else
+    map('n', '<leader>fs', '<cmd>tabedit ~/.config/nvim/lua/plugins.lua<cr>', opts)
 end
 -- Copy and Past from the system clipboard
 map('v', 'Y', '"+y', {})
 map('n', 'P', '"+p', {})
 
 -- Some functional configuration
-map('n', '<backspace>', '<cmd>noh<cr>', {}) -- cancel highlight that from search
+map('n', '<backspace>', '<cmd>noh<cr>', { desc = 'cancel highlight'}) -- cancel highlight that from search
 map('n', '-', '@q', {}) -- play the macro 'q'
 
 -- -- NvimTree
@@ -42,14 +44,26 @@ map("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", {});
 
 -- -- Telescope
 -- Find file
-map("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<CR>", {})
+map("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<CR>", { desc = 'find file'})
 -- Find word
-map("n", "<leader>fg", "<cmd>Telescope live_grep theme=dropdown<CR>", {})
+map("n", "<leader>fg", "<cmd>Telescope live_grep theme=dropdown<CR>", { desc = 'find word'})
 -- Find special sample
-map("n", "<leader>fb", "<cmd>Telescope buffers theme=dropdown<CR>", {})
+map("n", "<leader>fb", "<cmd>Telescope buffers theme=dropdown<CR>", { desc = 'find buffers'})
 -- check help doc
-map("n", "<leader>fh", "<cmd>Telescope help_tags theme=dropdown<CR>", {})
+map("n", "<leader>fh", "<cmd>Telescope help_tags theme=dropdown<CR>", { desc = 'check help doc'})
 -- check recent files
-map("n", "<leader>fr", "<cmd>Telescope oldfiles theme=dropdown<CR>", {})
+map("n", "<leader>fr", "<cmd>Telescope oldfiles theme=dropdown<CR>", { desc = 'check old files' })
 -- find marks
-map("n", "<leader>fm", "<cmd>Telescope marks theme=dropdown<CR>", {})
+map("n", "<leader>fm", "<cmd>Telescope marks theme=dropdown<CR>", { desc = 'find marks'})
+
+-- -- Dashboard
+map("n", "<leader>;", "<cmd>Alpha<CR>",{ desc = 'Dashboard'})
+
+local status_ok, wk = pcall(require, "which-key")
+if not status_ok then
+    return
+end
+wk.register({
+    ['<leader>f'] = { name = "+file"},
+    ['<leader>l'] = { name = '+lsp'}
+})
